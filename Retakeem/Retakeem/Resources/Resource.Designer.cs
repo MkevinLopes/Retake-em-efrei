16487086665c8fb6d7375d9f8041aa8964ea83a0
manuel.lopes <manuel.lopes@efrei.net>
2015-12-27 18:07:42 +0100

    Interface + compile X)

--- Retakeem/Retakeem/Resources/Resource.Designer.cs
+++ Retakeem/Retakeem/Resources/Resource.Designer.cs
@@ -61,10 +61,46 @@ namespace Retakeem
 		{
 			
 			// aapt resource value: 0x7f020000
-			public const int clap_board = 2130837504;
+			public const int Actualite = 2130837504;
 			
 			// aapt resource value: 0x7f020001
-			public const int Icon = 2130837505;
+			public const int Bouton_choix_aleatoire = 2130837505;
+			
+			// aapt resource value: 0x7f020002
+			public const int Chaise = 2130837506;
+			
+			// aapt resource value: 0x7f020003
+			public const int clap_board = 2130837507;
+			
+			// aapt resource value: 0x7f020004
+			public const int Classement = 2130837508;
+			
+			// aapt resource value: 0x7f020005
+			public const int Connexion = 2130837509;
+			
+			// aapt resource value: 0x7f020006
+			public const int Continuer = 2130837510;
+			
+			// aapt resource value: 0x7f020007
+			public const int Icon = 2130837511;
+			
+			// aapt resource value: 0x7f020008
+			public const int Jouer = 2130837512;
+			
+			// aapt resource value: 0x7f020009
+			public const int Options = 2130837513;
+			
+			// aapt resource value: 0x7f02000a
+			public const int Retakem_nom = 2130837514;
+			
+			// aapt resource value: 0x7f02000b
+			public const int RetakemLogo = 2130837515;
+			
+			// aapt resource value: 0x7f02000c
+			public const int Roulot_sur_le_cote_du_menu = 2130837516;
+			
+			// aapt resource value: 0x7f02000d
+			public const int Terminer = 2130837517;
 			
 			static Drawable()
 			{
@@ -79,35 +115,32 @@ namespace Retakeem
 		public partial class Id
 		{
 			
-			// aapt resource value: 0x7f070008
-			public const int MyButton = 2131165192;
-			
-			// aapt resource value: 0x7f07000b
-			public const int Record = 2131165195;
-			
 			// aapt resource value: 0x7f07000a
-			public const int SampleVideoView = 2131165194;
+			public const int Record = 2131165194;
 			
-			// aapt resource value: 0x7f070005
-			public const int continuer = 2131165189;
+			// aapt resource value: 0x7f070009
+			public const int SampleVideoView = 2131165193;
 			
 			// aapt resource value: 0x7f070003
 			public const int date = 2131165187;
 			
+			// aapt resource value: 0x7f070008
+			public const int editText1 = 2131165192;
+			
+			// aapt resource value: 0x7f070007
+			public const int editText2 = 2131165191;
+			
 			// aapt resource value: 0x7f070001
 			public const int email = 2131165185;
 			
-			// aapt resource value: 0x7f070006
-			public const int imageViewLogo = 2131165190;
+			// aapt resource value: 0x7f070005
+			public const int imageButton1 = 2131165189;
 			
-			// aapt resource value: 0x7f070009
-			public const int linearLayout1 = 2131165193;
+			// aapt resource value: 0x7f070006
+			public const int imageView1 = 2131165190;
 			
 			// aapt resource value: 0x7f070004
 			public const int linearLayoutbottom = 2131165188;
-			
-			// aapt resource value: 0x7f070007
-			public const int linearLayoutmid = 2131165191;
 			
 			// aapt resource value: 0x7f070000
 			public const int linearLayouttop = 2131165184;
